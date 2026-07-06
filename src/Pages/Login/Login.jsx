a674345e5c18d7b2a84f302b8a86b5d918283c0b
nisad2001 <nisadnn2001@gmail.com>
2026-07-06 16:13:18 +0600

    fix: adjust logo alignment and verification input sizing for mobile

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,16 @@
 import { PiSignIn } from "react-icons/pi";
+
 import { useForm } from "react-hook-form";
+
 import { useState } from "react";
+
 import { NavLink } from "react-router-dom";
+
+import { GoArrowRight } from "react-icons/go";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
+
   const [showPassword, setShowPassword] = useState(false);
 
   const onSubmit = (data) => {
@@ -15,6 +21,7 @@ const Login = () => {
     <div className='min-h-screen'>
       <div className='grid grid-cols-1 md:grid-cols-12 h-auto'>
         {/* Left side */}
+
         <div
           className="min-h-screen bg-[url('assets/home-img.png')] bg-cover bg-center bg-no-repeat md:col-span-7 flex flex-col justify-between pt-8 md:pt-0"
           style={{ backgroundImage: "url('/assets/home-img.png')" }}
@@ -24,6 +31,7 @@ const Login = () => {
               <NavLink to='/'>
                 <img src='assets/Rupai-logo.png' alt='Logo' />
               </NavLink>
+
               <p className='font-normal not-italic text-[14px] leading-[100%] text-[#1E1E1E]'>
                 Smart Management for Tea Estates
               </p>
@@ -33,7 +41,9 @@ const Login = () => {
               <h1 className='font-semibold text-[26px] md:text-[40px] leading-[100%] text-[#1E1E1E]'>
                 Manage Your Estate <br /> Smarter, Better, Together
               </h1>
+
               <div className='w-[82px] border-t-[6px] border-[#176C0E] mt-6 rounded-sm mx-auto md:mx-0'></div>
+
               <p className='text-[#1E1E1E] font-normal text-[18px] md:text-[20px] leading-[28px] mt-6'>
                 A complete ERP solution for Tea Estates to{" "}
                 <br className='hidden md:block' /> manage Plantation,
@@ -49,76 +59,91 @@ const Login = () => {
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Plantation Management
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf2.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Production Tracking
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf3.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Workforce Management
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf4.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-center text-white pt-2 text-[14px]'>
                   Real-time Reports
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf5.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Inventory Control
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf6.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Accounts & Finance
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf7.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   Sales & Auction
                 </p>
               </div>
+
               <div className='flex flex-col items-center w-[131px]'>
                 <img
                   src='assets/leaf8.png'
                   className='w-[72px] h-[72px]'
                   alt='leaf'
                 />
+
                 <p className='text-white pt-2 text-[14px] text-center'>
                   And More Modules
                 </p>
@@ -129,22 +154,30 @@ const Login = () => {
           <div className='bg-[#06470D] flex flex-col items-center justify-center w-full p-6 space-y-4 md:space-y-0 md:space-x-9 md:flex-row md:items-start'>
             <div className='flex items-center gap-[9px] text-white'>
               <img src='assets/shield-check.png' alt='shield' />
+
               <div className='text-center md:text-left'>
                 <p className='text-[16px]'>Secure & Reliable</p>
+
                 <p className='text-[12px]'>Your data is protected</p>
               </div>
             </div>
+
             <div className='flex items-center gap-[9px] text-white'>
               <img src='assets/shield-check.png' alt='shield' />
+
               <div className='text-center md:text-left'>
                 <p className='text-[16px]'>Trusted access</p>
+
                 <p className='text-[12px]'>Only you can access your account</p>
               </div>
             </div>
+
             <div className='flex items-center gap-[9px] text-white'>
               <img src='assets/shield-check.png' alt='shield' />
+
               <div className='text-center md:text-left'>
                 <p className='text-[16px]'>Real-time Protection</p>
+
                 <p className='text-[12px]'>Advanced security for your estate</p>
               </div>
             </div>
@@ -152,13 +185,25 @@ const Login = () => {
         </div>
 
         {/* Right side */}
-        <div className='col-span-12 md:col-span-5 w-full px-4 md:px-8 pb-10 flex flex-col items-center'>
-          <NavLink
-            to='/'
-            className='mt-4 bg-green-900 p-2 rounded-sm text-white cursor-pointer'
-          >
-            back to home
-          </NavLink>
+
+        <div className='col-span-12 md:col-span-5 w-full px-4 md:px-8 pb-10 flex flex-col items-center bg-base-100 shadow-sm '>
+          <div className='space-x-2 mt-4 flex '>
+            <NavLink
+              to='/'
+              className='border border-gray-300 py-3 rounded-lg font-semibold flex justify-center items-center gap-2 hover:bg-gray-50 transition p-8 hover:bg-green-900 hover:text-white'
+            >
+              ← go to home
+            </NavLink>
+
+            <NavLink
+              to='/two-factor'
+              className='border border-gray-300 py-3 rounded-lg font-semibold flex justify-center items-center gap-2 hover:bg-gray-50 transition p-8 hover:bg-green-900 hover:text-white'
+            >
+              got to two-factor
+              <GoArrowRight />
+            </NavLink>
+          </div>
+
           <div className='flex flex-col items-center text-center mt-[20px] md:mt-[110px] mb-8'>
             <div className='w-[90px] h-[90px] md:w-[129px] md:h-[129px] rounded-full border-[0.86px] border-[#AEAEAE] overflow-hidden flex items-center justify-center text-center mb-[20px]'>
               <img
@@ -167,10 +212,12 @@ const Login = () => {
                 className='w-[50px] h-[50px] md:w-[82px] md:h-[82px] object-cover'
               />
             </div>
+
             <div>
               <h1 className='font-semibold text-[#06470E] text-[22px] md:text-[32px] leading-[100%] tracking-[0] mb-[9px]'>
                 Welcome Back!
               </h1>
+
               <p className='text-black font-normal text-[14px] md:text-[18px] leading-[100%] tracking-[0]'>
                 Sign in to your <span className='text-[#06470E]'>RupAI</span>{" "}
                 ERP account
@@ -186,6 +233,7 @@ const Login = () => {
               <label className='block font-bold text-[18px] md:text-[20px] leading-[100%] tracking-[0] mb-[14px]'>
                 Username/E-mail
               </label>
+
               <input
                 {...register("identifier")}
                 className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#06470E]'
@@ -197,6 +245,7 @@ const Login = () => {
               <label className='block text-sm font-semibold mb-2'>
                 Password
               </label>
+
               <div className='relative'>
                 <input
                   type={showPassword ? "text" : "password"}
@@ -204,6 +253,7 @@ const Login = () => {
                   className='w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#06470E]'
                   placeholder='Enter your password'
                 />
+
                 <button
                   type='button'
                   onClick={() => setShowPassword(!showPassword)}
@@ -223,6 +273,7 @@ const Login = () => {
                 />{" "}
                 Remember me
               </label>
+
               <a href='#' className='text-[#06470E] font-medium'>
                 Forgot Password?
               </a>
@@ -234,6 +285,7 @@ const Login = () => {
             >
               <PiSignIn size={20} /> Sign In
             </button>
+
             <p className='font-normal text-base leading-none tracking-normal text-[#3D3B3B] text-center mt-[22px]'>
               or sign in with
             </p>
@@ -247,8 +299,10 @@ const Login = () => {
                     alt='google'
                   />
                 </button>
+
                 <p className='font-normal text-[14px] text-[#3D3B3B]'>google</p>
               </div>
+
               <div className='w-[142px] h-[116px] rounded-[9px] border border-[#CDCDCD] p-[10px] flex flex-col gap-[10px] items-center justify-center'>
                 <button type='button'>
                   <img
@@ -256,11 +310,14 @@ const Login = () => {
                     src='/assets/microsoft-btn.png'
                     alt='microsoft'
                   />
+                  theke mt nibe 40px
                 </button>
+
                 <p className='font-normal text-[14px] text-[#3D3B3B]'>
                   microsoft
                 </p>
               </div>
+
               <div className='w-[142px] h-[116px] rounded-[9px] border border-[#CDCDCD] p-[10px] flex flex-col gap-[10px] items-center justify-center'>
                 <button type='button'>
                   <img
@@ -269,6 +326,7 @@ const Login = () => {
                     alt='sso'
                   />
                 </button>
+
                 <p className='font-normal text-[14px] text-[#3D3B3B]'>SSO</p>
               </div>
             </div>
--- src/Pages/TwoFactor/TwoFactor.jsx
+++ src/Pages/TwoFactor/TwoFactor.jsx
@@ -2,29 +2,28 @@ import { NavLink } from "react-router-dom";
 
 const TwoFactor = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-12 min-h-screen'>
-      {/* Left side */}
-      <div className='md:col-span-7 relative'>
+    <div className='grid grid-cols-1 lg:grid-cols-12 min-h-screen'>
+      <div className='lg:col-span-7'>
         <div
-          className='min-h-screen bg-cover bg-center bg-no-repeat flex flex-col pt-8 md:pt-0'
+          className='h-full bg-cover bg-center bg-no-repeat flex flex-col justify-between'
           style={{ backgroundImage: "url('/assets/two-factor-bg.jpg')" }}
         >
-          <div className='mt-4 md:mt-12 ml-6 md:ml-24'>
+          <div className='mt-4 md:mt-12 ml-0 md:ml-24 flex flex-col items-center md:items-start'>
             <NavLink to='/'>
               <img src='assets/Rupai-logo.png' alt='Logo' />
             </NavLink>
-            <p className='font-normal not-italic text-[14px] leading-[100%] text-[#1E1E1E]'>
+            <p className='font-normal not-italic text-[14px] leading-[100%] text-[#1E1E1E] text-center md:text-left'>
               Smart Management for Tea Estates
             </p>
           </div>
 
-          <div className='flex-grow flex flex-col items-center justify-center px-6 md:px-24 py-10'>
+          <div className='flex flex-col items-center justify-center px-6 md:px-24 py-10'>
             <img
               src='/assets/Two-factor.jpg'
               alt='Two Factor'
               className='w-full max-w-[200px] md:max-w-[300px] h-auto rounded-full mb-8'
             />
-            <div className='text-center md:text-left w-full'>
+            <div className='text-center lg:text-left w-full'>
               <h1 className='font-semibold text-[22px] md:text-[32px] leading-[100%] mb-[9px] text-white'>
                 Extra security, <br /> Complete peace of mind
               </h1>
@@ -35,8 +34,7 @@ const TwoFactor = () => {
             </div>
           </div>
 
-          {/* Footer section fixed at bottom */}
-          <div className='bg-[#06470D] w-full p-6 grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4'>
+          <div className='bg-[#06470D] w-full p-8 grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4'>
             <div className='flex items-center gap-[9px] text-white justify-center md:justify-start'>
               <img src='assets/shield-check.png' alt='shield' />
               <div className='text-center md:text-left'>
@@ -62,14 +60,85 @@ const TwoFactor = () => {
         </div>
       </div>
 
-      {/* Right side */}
-      <div className='md:col-span-5 flex items-center justify-center p-8'>
-        <NavLink
-          to='/'
-          className='mt-4 bg-green-900 p-2 rounded-sm text-white cursor-pointer'
-        >
-          back to home
-        </NavLink>
+      <div className='lg:col-span-5 flex flex-col items-center justify-center p-4 md:p-6'>
+        <div className='w-full max-w-lg bg-white shadow-2xl rounded-2xl p-6 md:p-8 my-auto'>
+          <div className='flex flex-col items-center text-center mb-8'>
+            <img
+              src='/assets/two-factor-atuh.png'
+              alt=''
+              className='w-20 h-20 bg-green-50 rounded-full p-4'
+            />
+            <h1 className='font-semibold text-[22px] md:text-[32px] leading-[100%] mt-4 mb-4 text-green-800'>
+              Two-Factor Authentication
+            </h1>
+            <p className='font-normal text-[14px] md:text-[18px] leading-[100%] text-black'>
+              Please enter the verification code sent to your <br /> email and
+              mobile number
+            </p>
+          </div>
+
+          <div className='mb-6 text-center'>
+            <p className='font-normal text-[14px] mb-2 text-black text-left'>
+              Verification Code
+            </p>
+            {/* ইনপুট বক্সের সাইজ ছোট করা হয়েছে যেন এক লাইনে থাকে */}
+            <div className='flex flex-wrap gap-1 md:gap-2 justify-center'>
+              {[...Array(6)].map((_, i) => (
+                <input
+                  key={i}
+                  type='text'
+                  maxLength='1'
+                  className='w-9 h-10 md:w-14 md:h-16 border border-gray-300 rounded-lg text-center text-xl focus:border-green-600 outline-none'
+                />
+              ))}
+            </div>
+            <div className='flex justify-center items-center gap-2 text-green-800 font-medium my-4'>
+              <span>🕒 Code will expire in 02:45</span>
+            </div>
+          </div>
+
+          <div className='space-y-4'>
+            <div className='flex flex-col sm:flex-row items-center gap-2'>
+              <label className='input input-bordered flex items-center gap-2 w-full'>
+                📧
+                <input
+                  type='email'
+                  className='grow w-full'
+                  defaultValue='saadi@jamunatech.com'
+                />
+              </label>
+              <button className='text-green-700 font-semibold whitespace-nowrap self-end sm:self-center'>
+                Resend
+              </button>
+            </div>
+
+            <div className='flex flex-col sm:flex-row items-center gap-2'>
+              <label className='input input-bordered flex items-center gap-2 w-full'>
+                📞
+                <input
+                  type='tel'
+                  className='grow w-full'
+                  defaultValue='+880 1712 345 678'
+                />
+              </label>
+              <button className='text-green-700 font-semibold whitespace-nowrap self-end sm:self-center'>
+                Resend
+              </button>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-4 mt-8'>
+            <button className='bg-green-800 text-white py-3 rounded-lg font-semibold hover:bg-green-900 transition'>
+              Verify & Continue
+            </button>
+            <NavLink
+              to='/login'
+              className='border border-gray-300 py-3 rounded-lg font-semibold flex justify-center items-center gap-2 hover:bg-gray-50 transition'
+            >
+              ← Back to Login
+            </NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
